d3f3f5e04ddc2c44b84e71b18bafcd6fc040a130
krish14325 <krishnatalks156@gmail.com>
2026-05-18 20:07:27 +0530

    Fixed View Cart Button

--- cart.py
+++ cart.py
@@ -49,19 +49,21 @@ def viewcart(current_user):
     try:
         with open(f"{current_user}_user.txt","r") as f:
             data = f.readlines()
+            print("..................................")
+            print("These Are The Items In Your Cart")
+            print("..................................")
             for i in data:
                 parts = i.strip().split(" --> ")
                 part_id = parts[0]
                 part_name = parts[1]
                 part_price = parts[2]
                 part_stock = parts[3]
-            print("..................................")
-            print("These Are The Items In Your Cart")
-            print("..................................")
-            print("ID :",part_id)
-            print("Name :",part_name)
-            print("Price :",part_price)
-            print("Stock :",part_stock)
+                print("ID :",part_id)
+                print("Name :",part_name)
+                print("Price :",part_price)
+                print("Quantity :",part_stock)
+                print("..................................")
             print("Cart Finished")
     except FileNotFoundError:
         print("File Not Found")
+viewcart("Krishna")
