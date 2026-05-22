4e6bee8fe8d27bbfb01e4f24ff3c56eea5b49749
krish14325 <krishnatalks156@gmail.com>
2026-05-22 18:36:52 +0530

    Added update Feature

--- products.py
+++ products.py
@@ -35,19 +35,59 @@ def viewproducts():
             print("Stock :", parts[3])
 def searchproducts():
     products =input("Search : ")
+    foundlist=[]
     product_found = False
     try:
         with open("products.txt","r") as f:
             data =f.readlines()
         for i in data:
             parts =i.strip().split(" --> ")
-            if products.lower() == parts[1].lower():
+            if products.lower() in parts[1].lower():
                 product_found = True
-                break
-        if product_found:
-            print("Results :-")
-            print(i)
+                if product_found == True:
+                    foundlist.append(i)
+                else:
+                    pass
+        if len(foundlist) !=0:
+            for i in foundlist:
+                new_parts = i.strip().split(" --> ")
+                print("ID","-->",new_parts[0])
+                print("Name","-->",new_parts[1])
+                print("Price","-->",new_parts[2])
+                print("Stock","-->",new_parts[3])
         else:
-            print("Product Not Found")
+            print("No Product Found")
     except FileNotFoundError:
         print("File Not Found")
+def updateproduct():
+    products_id = input("Enter Product Id : ")
+    new_updates = []
+    product = False
+    with open("products.txt","r") as f:
+        data = f.readlines()
+        for i in data:
+            parts = i.strip().split(" --> ")
+            if products_id == parts[0]:
+                product = True
+                print("......................")
+                print(i)
+                print("......................")
+                new_id = input("Enter New Id :")
+                new_name = input("Enter New Name :")
+                new_price = input("Enter New Price :")
+                new_stock = input("Enter New Stock :")
+                new_updates.append(f"{new_id} --> {new_name} --> {new_price} --> {new_stock}\n")
+            else:
+                new_updates.append(i)
+    if product:
+        with open("products.txt","w") as f:
+            data = f.writelines(new_updates)
+        print("Product Updated Sucessfully")
+    else:
+        print("Product Not Found")
+            
+
+        
+        
+        
+    
